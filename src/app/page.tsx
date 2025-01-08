60ab9372e2dc6cfe18ffb166f830356d3b51996c
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2025-01-09 02:33:33 +0800

    Tech/seo (#7)
    * tech(seo): add schema and meta data
    
    * tech(seo): add metadata and sitemap and robot
    
    * tech(seo): add vercel analytics
    
    * tech(seo): add google analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,52 @@
-
 import Header from "@/components/Header";
 import HeroSection from "@/app/_homesections/HeroSection";
 import SkillsSection from "@/app/_homesections/SkillsSection";
 import ProjectsSection from "@/app/_homesections/ProjectsSection";
 import Footer from "@/components/Footer";
+import Head from "next/head";
+import { projects } from "@/data/project";
 
 export default function Home() {
-  return (
-      <div>
-          <Header/>
-          <section className="flex flex-col gap-12 lg:gap-20">
-              <HeroSection/>
-              <SkillsSection/>
-              <ProjectsSection/>
-              <Footer/>
-          </section>
+    const structuredData = {
+        "@context": "https://schema.org",
+        "@type": "WebPage",
+        "name": "Thirdy Gayares - Software Engineer in the Philippines",
+        "description": "Explore the projects and portfolio of Thirdy Gayares, showcasing expertise in mobile apps, backend systems, and web development.",
+        "url": "https://thirdygayares.com",
+        "author": {
+            "@type": "Person",
+            "name": "Thirdy Gayares",
+            "url": "https://www.linkedin.com/in/thirdygayares/",
+        },
+        "projects": projects.map((project) => ({
+            "@type": "CreativeWork",
+            "name": project.name,
+            "description": project.description,
+            "image": `https://thirdygayares.com/image/project-cover/${project.image}`,
+            "url": "https://thirdygayares.com/#project",
+        })),
+    };
 
-      </div>
-  );
+    return (
+        <>
+            <Head>
+                <script
+                    type="application/ld+json"
+                    dangerouslySetInnerHTML={{
+                        __html: JSON.stringify(structuredData),
+                    }}
+                />
+            </Head>
+
+            <Header />
+            <main>
+                <article className="flex flex-col gap-12 lg:gap-20">
+                    <HeroSection />
+                    <SkillsSection />
+                    <ProjectsSection />
+                    <Footer />
+                </article>
+            </main>
+        </>
+    );
 }
---
+++ src/app/sitemap.ts
@@ -0,0 +1,10 @@
+export default async function sitemap(){
+    return[
+        {
+            url: "https://thirdygayares.com",
+            lastModified: new Date(),
+            priority: 1,
+            changeFrequency: "daily",
+        }
+    ]
+}
