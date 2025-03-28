ed0e46da67b193069e4260bd3d83029e64c455c8
thirdygayares <gayaresthird@gmail.com>
2025-03-29 00:09:26 +0800

    refactor: enhance ImageModal and streamline ProjectSection
    Added an optional `altName` prop to ImageModal for better accessibility. Updated ProjectCard to pass project name as alt text when opening the modal. Refactored ProjectsSection to improve readability and maintainability.

--- src/app/_homesections/ProjectsSection.tsx
+++ src/app/_homesections/ProjectsSection.tsx
@@ -8,9 +8,11 @@ import ImageModal from "@/components/ImageModal";
 const ProjectsSection = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [modalImage, setModalImage] = useState("");
+    const [modalAlt, setModalAlt] = useState("");
 
-    const openModal = (image: string) => {
+    const openModal = (image: string, alt:string) => {
         setModalImage(image);
+        setModalAlt(alt);
         setIsModalOpen(true);
     };
 
@@ -19,16 +21,23 @@ const ProjectsSection = () => {
         setModalImage("");
     };
 
+    const projectList = projects.map((project) => {
+        const {name} = project;
+        return(
+            <ProjectCard key={name} project={project} onImageClick={openModal} />
+        )
+    })
+
     return (
         <section id="project" className="container m-auto px-8 xl:px-0 flex flex-col gap-3 scroll-mt-24">
             <h2 className="text-center text-xl font-bold mb-4">Project</h2>
             <div className="flex flex-col lg:grid lg:grid-cols-2 gap-5">
-                {projects.map((project) => (
-                    <ProjectCard key={project.name} project={project} onImageClick={openModal} />
-                ))}
+                {projectList}
             </div>
 
-            {isModalOpen && <ImageModal image={modalImage} onClose={closeModal} />}
+            {isModalOpen &&
+                <ImageModal image={modalImage} onClose={closeModal} altName={modalAlt} />
+            }
         </section>
     );
 };
