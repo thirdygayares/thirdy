95968356f083f7dd40c6c5379eec847a2389abae
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2025-03-29 01:03:50 +0800

    feat(code-component): Add syntax highlighting component and update page layout (#10)
    Introduce a new `CodeComponent` using react-syntax-highlighter for code presentation with clipboard copying functionality. Update the `page.tsx` layout by wrapping content in a styled container with a background color for improved UI. Add related dependencies such as `react-syntax-highlighter` and `@heroicons/react` to support the new features.

---
+++ src/components/CodeComponent.tsx
@@ -0,0 +1,38 @@
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
+import {useState} from "react";
+import {ClipboardDocumentCheckIcon, ClipboardIcon} from "@heroicons/react/24/solid";
+
+interface CodeComponentProps{
+    code: string;
+    language?: string;
+}
+
+const CodeComponent = (codeComponentProps: CodeComponentProps) => {
+    const { code, language="javascript" } = codeComponentProps;
+
+    const [copied, setCopied] = useState(false);
+
+    const copyToClipboard = async () => {
+        await navigator.clipboard.writeText(code);
+        setCopied(true);
+        setTimeout(() => setCopied(false), 2000);
+    };
+
+
+    return (
+        <div className="relative bg-gray-800  text-white p-4 rounded-lg overflow-x-auto w-full">
+            <button
+                onClick={copyToClipboard}
+                className="absolute top-2 right-2 bg-gray-700 text-white p-2 rounded text-sm hover:bg-gray-600 transition"
+            >
+                {copied ? <ClipboardDocumentCheckIcon className="size-4" /> : <ClipboardIcon className="size-4" />}
+            </button>
+            <SyntaxHighlighter language={language} style={dracula} >
+                {code}
+            </SyntaxHighlighter>
+        </div>
+    );
+}
+
+export default CodeComponent;
